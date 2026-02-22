dc2646e7527d61422c492a7bc404161af16fd538
Remi <remidalise@yahoo.fr>
2026-02-22 13:41:25 +0100

    fix csp

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline';",
+          },
+        ],
+      },
+    ]
+  },
+}
 module.exports = nextConfig
